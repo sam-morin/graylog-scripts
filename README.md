6af4ad776125ad5266245b0437ce060a5470513f
sam-morin <sammorin@live.com>
2023-10-21 19:13:18 -0400

    minor improvements - add params/args

--- README.md
+++ README.md
@@ -4,8 +4,16 @@ Deploy on Ubuntu (prompt for sudo)
 ```shell
 curl -sSL https://raw.githubusercontent.com/sam-morin/graylog-scripts/main/rsyslog-ubuntu.sh | bash
 ```
+Using custom server and port as arguments:
+```shell
+curl -sSL https://raw.githubusercontent.com/sam-morin/graylog-scripts/main/rsyslog-ubuntu.sh | bash -s "10.10.5.3" "514"
+```
 
 Deploy on Ubuntu (don't prompt for sudo, user is already root (in the case sudo doesn't exist on the system)):
 ```shell
 curl -sSL https://raw.githubusercontent.com/sam-morin/graylog-scripts/main/rsyslog-ubuntu-root.sh | bash
+```
+Using custom server and port as arguments:
+```shell
+curl -sSL https://raw.githubusercontent.com/sam-morin/graylog-scripts/main/rsyslog-ubuntu-root.sh | bash -s "10.10.5.3" "514"
 ```
--- rsyslog-ubuntu.sh
+++ rsyslog-ubuntu.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-GRAYLOG_SERVER_IP="192.168.1.14"
-RSYSLOG_CONF="/etc/rsyslog.conf"
+# Define default values
+DEFAULT_GRAYLOG_SERVER_IP="192.168.1.14"
+DEFAULT_GRAYLOG_SERVER_PORT="1514"
+
+# Use command-line arguments or default values
+GRAYLOG_SERVER_IP="${1:-$DEFAULT_GRAYLOG_SERVER_IP}"
+GRAYLOG_SERVER_PORT="${2:-$DEFAULT_GRAYLOG_SERVER_PORT}"
 
 # Check if rsyslog is installed
 if ! command -v rsyslogd &> /dev/null; then
@@ -9,18 +14,21 @@ if ! command -v rsyslogd &> /dev/null; then
     sudo apt install rsyslog -y
 fi
 
+# The location of the new rsyslog.conf file. The file will be created by this script.
+RSYSLOG_CONF="/etc/rsyslog.conf"
+
 # Backup the rsyslog configuration file
 sudo cp "$RSYSLOG_CONF" "$RSYSLOG_CONF.bak"
 
 # Add configuration to forward logs to Graylog
-echo "*.emerg @${GRAYLOG_SERVER_IP}:1514" | sudo tee -a "$RSYSLOG_CONF" > /dev/null
-echo "*.alert @${GRAYLOG_SERVER_IP}:1514" | sudo tee -a "$RSYSLOG_CONF" > /dev/null
-echo "*.crit @${GRAYLOG_SERVER_IP}:1514" | sudo tee -a "$RSYSLOG_CONF" > /dev/null
-echo "*.err @${GRAYLOG_SERVER_IP}:1514" | sudo tee -a "$RSYSLOG_CONF" > /dev/null
-echo "*.warning @${GRAYLOG_SERVER_IP}:1514" | sudo tee -a "$RSYSLOG_CONF" > /dev/null
+echo "*.emerg @${GRAYLOG_SERVER_IP}:${GRAYLOG_SERVER_PORT}" | sudo tee -a "$RSYSLOG_CONF" > /dev/null
+echo "*.alert @${GRAYLOG_SERVER_IP}:${GRAYLOG_SERVER_PORT}" | sudo tee -a "$RSYSLOG_CONF" > /dev/null
+echo "*.crit @${GRAYLOG_SERVER_IP}:${GRAYLOG_SERVER_PORT}" | sudo tee -a "$RSYSLOG_CONF" > /dev/null
+echo "*.err @${GRAYLOG_SERVER_IP}:${GRAYLOG_SERVER_PORT}" | sudo tee -a "$RSYSLOG_CONF" > /dev/null
+echo "*.warning @${GRAYLOG_SERVER_IP}:${GRAYLOG_SERVER_PORT}" | sudo tee -a "$RSYSLOG_CONF" > /dev/null
 
 # Restart rsyslog
 sudo service rsyslog restart
 
-echo "rsyslog has been configured to forward logs to Graylog at ${GRAYLOG_SERVER_IP}:1514."
-echo "Please verify that Graylog is correctly configured to receive syslog messages."
+echo "rsyslog has been configured to forward logs to Graylog at ${GRAYLOG_SERVER_IP}:${GRAYLOG_SERVER_PORT}."
+echo "Please verify that Graylog is correctly configured to receive syslog messages by checking for events in Graylog."
